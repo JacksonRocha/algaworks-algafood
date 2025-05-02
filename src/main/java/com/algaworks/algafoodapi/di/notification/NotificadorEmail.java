6e1ea02102ae14e2b8705d9ce935113781551126
jacksonrocha <jackson_lbr@hotmail.com>
2025-05-01 21:28:14 -0300

    2.11 Criandos dos Beans do spring (@Service @Component)

---
+++ src/main/java/com/algaworks/algafoodapi/di/notification/NotificadorEmail.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodapi.di.notification;
+
+import com.algaworks.algafoodapi.di.model.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificadorEmail {
+
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("Notificando %s através do e-mail %s: %s\n",
+                cliente.getNome(), cliente.getEmail(), mensagem);
+    }
+}
