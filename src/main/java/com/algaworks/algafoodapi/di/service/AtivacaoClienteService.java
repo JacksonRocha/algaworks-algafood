2a448161cc7eb9981bf47c376934cfd0cba36a04
jacksonrocha <jackson_lbr@hotmail.com>
2025-05-01 21:28:03 -0300

    2.11 Criandos dos Beans do spring (@Service @Component)

---
+++ src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafoodapi.di.service;
+
+import com.algaworks.algafoodapi.di.model.Cliente;
+import com.algaworks.algafoodapi.di.notification.NotificadorEmail;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AtivacaoClienteService {
+
+    private NotificadorEmail notificador;
+
+    public void ativar(Cliente cliente) {
+        cliente.ativar();
+
+        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+    }
+}
