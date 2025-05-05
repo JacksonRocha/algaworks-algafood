cca17de070a9db392a24314cfcbb06ef8c826562
jacksonrocha <jackson_lbr@hotmail.com>
2025-05-05 20:45:55 -0300

    3.3 Adicionando JPA e configurando o Data Source

--- src/main/java/com/algaworks/algafoodapi/di/notification/NivelUrgencia.java
+++
@@ -1,6 +0,0 @@
-package com.algaworks.algafoodapi.di.notification;
-
-public enum NivelUrgencia {
-    URGENTE,
-    SEM_URGENCIA
-}
--- src/main/java/com/algaworks/algafoodapi/di/notification/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.algafoodapi.di.notification;
-
-import com.algaworks.algafoodapi.di.model.Cliente;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface Notificador {
-    void notificar(Cliente cliente, String mensagem);
-}
