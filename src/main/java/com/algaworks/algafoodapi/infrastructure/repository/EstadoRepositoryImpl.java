37bd6022ed98e388e5aded548dc124ebf5cc0044
jacksonrocha <jackson_lbr@hotmail.com>
2025-06-01 14:32:34 -0300

    3.20 Desafio: mapeando entidades

---
+++ src/main/java/com/algaworks/algafoodapi/infrastructure/repository/EstadoRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.algaworks.algafoodapi.infrastructure.repository;
+
+import com.algaworks.algafoodapi.domain.model.Estado;
+import com.algaworks.algafoodapi.domain.repository.EstadoRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class EstadoRepositoryImpl implements EstadoRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Estado> listar() {
+        return manager.createQuery("from Estado", Estado.class)
+                .getResultList();
+    }
+
+    @Override
+    public Estado buscar(Long id) {
+        return manager.find(Estado.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Estado salvar(Estado estado) {
+        return manager.merge(estado);
+    }
+
+    @Transactional
+    @Override
+    public void remover(Estado estado) {
+        estado = buscar(estado.getId());
+        manager.remove(estado);
+    }
+}
